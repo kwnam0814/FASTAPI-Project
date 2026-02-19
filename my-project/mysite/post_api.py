724bedc114a27b84936c4adc1817f9ecb6510403
kwnam0814 <kwnam0814@naver.com>
2026-02-19 11:27:17 +0900

    간단한 게시판 구현 : 공통 URL 경로 설정

--- my-project/mysite/post_api.py
+++ my-project/mysite/post_api.py
@@ -3,15 +3,18 @@
 # 각 게시글의 고유한 식별 번호를 위한 변수 만들기.
 # FastAPI의 본체인 main.py와 분리되었으므로, Endpoint를 연결할 Router를 생성.
 
-# Restful한 게시판 구현
-# Restful한 API는 자원은 URL로 표현하고, 행위는 HTTP 메서드로 표현
+# 공통 URL 경로 설정
+# 여러 엔드포인트에서 공통적으로 사용하는 경로를 통합하여 관리.
+# FastAPI에서는 APIRouter 인스턴스 생성 시 경로 접두사를 설정하여 코드 중복을 제거.
+# 하위 메서드에서는 공통 경로를 제외한 나머지 경로만 정의함.
+# tag를 통해 swagger를 그룹화할 수 있음.
 
 # mysite/post_api.py
 
 from fastapi import APIRouter
 from .post import Post
 
-router = APIRouter()
+router = APIRouter(prefix="/posts", tags=["Post"])
 
 # 게시글 객체들을 저장하는 리스트
 posts = []
@@ -22,9 +25,8 @@ posts.append(Post(1, "기본 제목", "기본 내용"))
 post_id = 1
 
 
-@router.post("/posts/create")
+@router.post("")
 def create_post():
-    # 식별자 1 증가
     global post_id
     post_id += 1
 
@@ -36,13 +38,13 @@ def create_post():
     return post
 
 
-@router.get("/posts")
+@router.get("")
 def read_posts():
     # 게시글 리스트 전체를 반환
     return posts
 
 
-@router.get("/posts/{id}")
+@router.get("/{id}")
 def read_post_by_id(id: int):
     # 리스트 내 객체들을 하나씩 검사
     for post in posts:
@@ -52,7 +54,7 @@ def read_post_by_id(id: int):
     return None
 
 
-@router.put("/posts/{id}")
+@router.put("/{id}")
 def update_post(id: int):
     for post in posts:
         if post.id == id:
@@ -63,7 +65,7 @@ def update_post(id: int):
     return None
 
 
-@router.delete("/posts/{id}")
+@router.delete("/{id}")
 def delete_post(id: int):
     # 리스트를 순회하며 인덱스(i)와 내용(post)을 함께 추출
     for i, post in enumerate(posts):
