175eec98dba6099f6f48424b0e914fb3973ddfb3
kwnam0814 <kwnam0814@naver.com>
2026-02-19 11:04:45 +0900

    UPDATE : main.py

--- my-project/main.py
+++ my-project/main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from my-project!")
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
