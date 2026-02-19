0c5644b42f5bebe2a9e25bc0a305f616b7d82f77
kwnam0814 <kwnam0814@naver.com>
2026-02-19 11:24:52 +0900

    간단한 게시판 구현

--- my-project/mysite/post_api.py
+++ my-project/mysite/post_api.py
@@ -3,6 +3,9 @@
 # 각 게시글의 고유한 식별 번호를 위한 변수 만들기.
 # FastAPI의 본체인 main.py와 분리되었으므로, Endpoint를 연결할 Router를 생성.
 
+# Restful한 게시판 구현
+# Restful한 API는 자원은 URL로 표현하고, 행위는 HTTP 메서드로 표현
+
 # mysite/post_api.py
 
 from fastapi import APIRouter
@@ -14,18 +17,17 @@ router = APIRouter()
 posts = []
 post_id = 0
 
-# Create - 게시글 작성
-# 특정 주소로 요청을 보낼 때 새로운 게시글 객체를 생성하고 리스트에 추가.
-# mysite/post_api.py 내부에 추가
+# 초기 데이터 추가를 위한 구문
+posts.append(Post(1, "기본 제목", "기본 내용"))
+post_id = 1
 
 
-@router.get("/posts/create")
+@router.post("/posts/create")
 def create_post():
     # 식별자 1 증가
     global post_id
     post_id += 1
 
-    # 새로운 게시글 객체 생성
     post = Post(post_id, "제목", "내용")
 
     # 리스트에 추가
@@ -34,38 +36,10 @@ def create_post():
     return post
 
 
-## Read - 전체 게시글 조회
-# 현재 리스트에 저장된 모든 게시글 데이터를 반환.
-# 데이터 확인을 위해 초기화 로직을 추가할 수 있음.
-# mysite/post_api.py 내부에 추가
-
-# 초기 데이터 추가를 위한 구문
-posts.append(Post(1, "기본 제목", "기본 내용"))
-post_id = 1
-
-
 @router.get("/posts")
 def read_posts():
     # 게시글 리스트 전체를 반환
     return posts
-
-
-# Read - 단일 게시글 조회
-# 단일 게시글을 식별하기 위한 id가 필요.
-
-
-# 경로 매개변수(path parameter)
-# 주소 경로에 변수를 포함하여 데이터를 전달하는 방식.
-# 중괄호를 사용하여 변수명을 지정.
-@router.get("/articles/{id}")
-def get_article(id: int):
-    # 매개변수로 전달받은 id를 활용
-    return f"글 번호: {id}"
-
-
-# 단일 게시글 조회
-# 리스트를 순회하며 요청받은 식별자와 일치하는 객체를 탐색.
-# mysite/post_api.py 내부에 추가
 
 
 @router.get("/posts/{id}")
@@ -78,13 +52,7 @@ def read_post_by_id(id: int):
     return None
 
 
-# Update - 게시글 수정
-# 특정 식별자의 게시글을 찾아 내부 데이터를 변경.
-
-# mysite/post_api.py 내부에 추가
-
-
-@router.get("/posts/{id}/update")
+@router.put("/posts/{id}")
 def update_post(id: int):
     for post in posts:
         if post.id == id:
@@ -95,19 +63,13 @@ def update_post(id: int):
     return None
 
 
-# 게시글 삭제
-# 특정 식별자와 일치하는 게시글을 찾아 리스트에서 제거.
-
-# mysite/post_api.py 내부에 추가
-
-
-@router.get("/posts/{id}/delete")
+@router.delete("/posts/{id}")
 def delete_post(id: int):
     # 리스트를 순회하며 인덱스(i)와 내용(post)을 함께 추출
     for i, post in enumerate(posts):
         if post.id == id:
             # 해당 인덱스의 데이터를 삭제하고 루프 종료
-
-            return posts.pop(i)
+            posts.pop(i)
+            return "삭제 완료"
 
     return "삭제 실패"
